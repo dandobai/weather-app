9c5140199f75f7b518446236c90ca78b8c7ce57a
dandobai <dandobai@gmail.com>
2023-02-24 22:38:13 +0100

    update

--- src/main/java/com/example/weatherapp/services/WeatherInfoServiceImp.java
+++ src/main/java/com/example/weatherapp/services/WeatherInfoServiceImp.java
@@ -9,7 +9,6 @@ import org.springframework.stereotype.Service;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 
 @Service
